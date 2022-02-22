2f12393b0480be4eda852c3688266077c81b91ce
Leene <marleenstueber@web.de>
2022-02-22 14:07:27 +0100

    Updates

---
+++ precache-manifest.f5c9662977e6be7bef36dd91e3f87862.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a9ae4d778e4fc615876e3c8e863513e",
+    "url": "/typescript-infographic/index.html"
+  },
+  {
+    "revision": "9e2311acf6ffde525201",
+    "url": "/typescript-infographic/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5caeaae02598237feb23",
+    "url": "/typescript-infographic/static/js/2.ea490775.chunk.js"
+  },
+  {
+    "revision": "bd81e5c6feaf63e9a711",
+    "url": "/typescript-infographic/static/js/3.4183067e.chunk.js"
+  },
+  {
+    "revision": "9e2311acf6ffde525201",
+    "url": "/typescript-infographic/static/js/main.6b71928a.chunk.js"
+  },
+  {
+    "revision": "a1efd6e1490b9bfbac76",
+    "url": "/typescript-infographic/static/js/runtime~main.b0123729.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/typescript-infographic/static/media/logo.06e73328.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1ab02acdae6af6ec4bfe1ebd52cc9c39.js"
+  "/typescript-infographic/precache-manifest.f5c9662977e6be7bef36dd91e3f87862.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/typescript-infographic/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
